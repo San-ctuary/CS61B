899804b942a318d8a1490d6b88ee919996c3652a
Codingboy <836790896@qq.com>
2021-05-27 23:45:24 +0800

    finish proj1b

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -13,12 +13,15 @@ public class TestOffByOne {
 //    Uncomment this class once you've created your CharacterComparator interface and OffByOne class. **/
     @Test
     public void testOffByOne() {
-//    	assertTrue(offByOne.equalChars('b', 'a'));
-//        assertFalse(offByOne.equalChars('a', 'a'));
-//        assertFalse(offByOne.equalChars('a', ' '));
-//        assertFalse(offByOne.equalChars('a', 'b'));
-//    	assertTrue(offByOne.equalChars('a', 'A'));
-    			
+    	assertTrue(offByOne.equalChars('b', 'a'));
+        assertFalse(offByOne.equalChars('a', 'a'));
+        assertFalse(offByOne.equalChars('a', ' '));
+        assertTrue(offByOne.equalChars('a', 'b'));
+    	assertFalse(offByOne.equalChars('a', 'A'));
+        assertTrue(offByOne.equalChars('r', 'q'));
+        assertTrue(offByOne.equalChars('%', '&'));
+
+
     }
     @Test
     public void testOffByOne2(){
